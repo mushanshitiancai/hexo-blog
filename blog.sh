71374bbe510a4540ebd97ffe8834324b4472cd4c
mazhibin <mazhibin@ppzuche.com>
2016-03-15 19:31:37 +0800

    update blog.sh

--- blog.sh
+++ blog.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+# author: mazhibin
+# log   : 2016-02-25 新建
+#       : 2016-03-15 获取git仓库状态，只在有修改的情况下，才会执行发布操作
+
+
+if [ "$#" = 0 || "$1" = "-h" ];then
+  echo "Usage: sh blog.sh [dnp]"
+  echo "       d  deployment hexo to github"
+  echo "       n  create a new blog"
+  echo "       p  publish draft to post"
+fi
 
 cd "$(dirname "$0")"
 
+# 判断git仓库是否有未提交的修改
 function git_change(){
   expr $(git status --porcelain 2>/dev/null | wc -l)
 }
 
+# 发布最新博客到github，同时也把hexo参考提交到github
+# 如果没有修改则不会进行提交
 if [ "$1" = "d" ];then
     if [ ! git_change = "0" ];then
       echo "not modified."
@@ -18,6 +32,7 @@ if [ "$1" = "d" ];then
     echo -e "deploy `date +%Y-%m-%d\ %H:%M:%S`\n" >> ~/blog_log
 fi
 
+# 新建博客，也可以新建草稿
 if [ "$1" = "n" ];then
     if [ "$#" = "3" ];then
         hexo n "$2" "$3"
@@ -26,7 +41,7 @@ if [ "$1" = "n" ];then
     fi
 fi
 
-
+# 发布草稿到正式博客
 if [ "$1" = "p" ];then
     hexo p "$2"
 fi
